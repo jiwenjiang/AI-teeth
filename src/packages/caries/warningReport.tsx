634f9e4b12d6bf13d08f3119856bcb9e3ec74614
jiwenjiang <513755565@qq.com>
2022-12-24 16:51:08 +0800

    warning-report

--- src/packages/caries/warningReport.tsx
+++ src/packages/caries/warningReport.tsx
@@ -1,15 +1,18 @@
 import NavBar from "@/comps/NavBar";
 import { SystemContext } from "@/service/context";
 import request from "@/service/request";
+import baocun from "@/static/icons/baocun.svg";
 import Current1 from "@/static/icons/currentGreen.svg";
 import Current3 from "@/static/icons/currentRed.svg";
 import Current2 from "@/static/icons/currentYellow.svg";
 import Female from "@/static/icons/female.png";
-import Issue from "@/static/icons/warningreport-problems-bg.png";
+import fenxiang from "@/static/icons/fenxiang.svg";
 import Male from "@/static/icons/male.png";
 import Voice from "@/static/icons/voice.svg";
+import Issue from "@/static/icons/warningreport-problems-bg.png";
 import Tishi from "@/static/imgs/weixintishi.png";
-import { Image, ScrollView, Text, View } from "@tarojs/components";
+import { Popup } from "@taroify/core";
+import { Button, Image, ScrollView, Text, View } from "@tarojs/components";
 import { getCurrentPages, navigateBack, useRouter } from "@tarojs/taro";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { cls } from "reactutils";
@@ -28,6 +31,8 @@ export default function App() {
   const [navBarTitle] = useState(router.params.childName ?? "儿童龋齿检测");
   const [data, setData] = useState<any>({});
   const canvasBox = useRef();
+  const [show, setShow] = useState(false);
+  const [reportImg, setReportImg] = useState("");
 
   const onNavBarClick = () => {
     const currentPages = getCurrentPages();
@@ -52,13 +57,56 @@ export default function App() {
     });
     setData({
       ...response.data,
-      problems: response.data.problems.filter((v: any) => v),
+      problems: response.data.problems.filter((v: any) => v)
     });
   };
 
   useEffect(() => {
     getAttr();
   }, []);
+
+  const save = async () => {
+    setShow(true);
+    const res = await request({
+      url: "/check/report",
+      data: { id: router.params.id }
+    });
+    setReportImg(res.data?.replace(/[\r\n]/g, ""));
+  };
+
+  const saveImg = () => {
+    if (!reportImg) {
+      wx.showToast({
+        title: "图片生成中",
+        icon: "loading"
+      });
+      return;
+    }
+    const fileSystem = wx.getFileSystemManager();
+    const time = new Date().valueOf();
+    fileSystem.writeFile({
+      filePath: wx.env.USER_DATA_PATH + `/${time}.png`,
+      data: reportImg,
+      encoding: "base64",
+      success: res => {
+        wx.saveImageToPhotosAlbum({
+          filePath: wx.env.USER_DATA_PATH + `/${time}.png`,
+          success: function(res) {
+            wx.showToast({
+              title: "保存成功"
+            });
+          },
+          fail: function(err) {
+            console.log(err);
+          }
+        });
+        console.log(res);
+      },
+      fail: err => {
+        console.log(err);
+      }
+    });
+  };
 
   return (
     <View className="page">
@@ -121,21 +169,68 @@ export default function App() {
             <View className={styles.listHead}>
               <View className={styles.border}></View>
               <View className={styles.titleBox}>
-                <Image src={Issue} mode='widthFix' className={styles.issue}></Image>
+                <Image
+                  src={Issue}
+                  mode="widthFix"
+                  className={styles.issue}
+                ></Image>
                 <Text className={styles.issueText}>问题清单</Text>
               </View>
               <View className={styles.hLine}></View>
               <View className={styles.dot}></View>
             </View>
             <View className={styles.listBody}>
-              {data?.problems?.length > 0 ? data.problems.map((v, i) => (
-                <View key={i} className={styles.listItem}>
-                  <View className={styles.itemBg}>{v}</View>
-                </View>
-              )) : <View className={styles.noProblem}>牙齿情况正常</View>}
+              {data?.problems?.length > 0 ? (
+                data.problems.map((v, i) => (
+                  <View key={i} className={styles.listItem}>
+                    <View className={styles.itemBg}>{v}</View>
+                  </View>
+                ))
+              ) : (
+                <View className={styles.noProblem}>牙齿情况正常</View>
+              )}
             </View>
           </View>
         </ScrollView>
+        <View className={cls(styles.btn)} onClick={save}>
+          保存/分享
+        </View>
+        <Popup
+          defaultOpen
+          placement="bottom"
+          open={show}
+          onClose={() => setShow(false)}
+        >
+          <View className={styles.shareBox}>
+            <View className={styles.shareIconBox}>
+              <Button openType="share" className={styles.shareBtn}>
+                <View>
+                  <View className={styles.iconBox}>
+                    <Image src={fenxiang} className={styles.icon} />
+                  </View>
+                  <View className={styles.iconText}>分享</View>
+                </View>
+              </Button>
+
+              <View onClick={saveImg}>
+                <View className={styles.iconBox}>
+                  <Image src={baocun} className={styles.icon} />
+                </View>
+                <View className={styles.iconText}>保存</View>
+              </View>
+            </View>
+            <View className={styles.cancel} onClick={() => setShow(false)}>
+              取消
+            </View>
+          </View>
+          <View className={styles.genImgBox}>
+            <Image
+              src={reportImg}
+              mode="widthFix"
+              className={styles.reportImg}
+            />
+          </View>
+        </Popup>
       </View>
     </View>
   );
